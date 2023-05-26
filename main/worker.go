1189c96c2905ff58f9e6e515d62f1b6598089353
root <root@LAPTOP-C1P8KR5S.localdomain>
2023-05-26 15:41:50 +0700

    initial commit

---
+++ main/worker.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Data struct {
+	Name   string `json: "firstName"`
+	Email  string `json: "email"`
+	Origin string `json: "origin"`
+}
+
+func NewData(name, email, origin string) *Data {
+	return &Data{
+		Name:   name,
+		Email:  email,
+		Origin: origin,
+	}
+}
+
+func InsertMyJson() {
+
+	var payload []Data
+
+	newData := NewData("Samuel", "samuel@gmail.com", "Jakarta")
+	for i := 0; i < 100000; i++ {
+		payload = append(payload, *newData)
+	}
+
+	convJson, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("../DBT/myjson.json", convJson, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func InsertSecJson() {
+
+	var payload []Data
+
+	newData := NewData("Samuel", "samuel@gmail.com", "Jakarta")
+
+	for i := 0; i < 100000; i++ {
+		payload = append(payload, *newData)
+	}
+
+	convJson, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("../DBT/secjson.json", convJson, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	InsertMyJson()
+	InsertSecJson()
+}
